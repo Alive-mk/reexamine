1b7ad5b95d55eef95c8f3e09f5d28773129dc150
BIT-Ming <2924565971@qq.com>
2024-09-20 11:03:33 +0800

    二分查找

---
+++ 二分查找.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int bysearch(vector<int> a, int num, int &cnt){
+	int left = 0;
+	int right = a.size() - 1;
+	while(left <= right){
+		cnt++;
+		if(left == right){
+			if(a[left] == num || a[right] == num){
+				return left;
+			}
+			else{
+				return -1;
+			}
+		}
+		int mid = (right + left) / 2;
+		if(a[mid] == num){
+			return mid;
+		}
+		else if(a[mid] < num){
+			left = mid + 1;
+		}
+		else{
+			right = mid - 1;
+		}
+	}
+}
+
+int main(){
+	int tmp;
+	int cnt=0;
+	vector<int> numbers;
+	while(1){
+		scanf("%d", &tmp);
+		numbers.push_back(tmp);
+		if(getchar() == '\n'){//一种比较好的输入办法  a good way to input 
+			break;
+		}
+	}
+	int num;
+	cin>> num;
+	int pos = bysearch(numbers, num, cnt);
+	cout<<pos<<endl;
+}
